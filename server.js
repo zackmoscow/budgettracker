e8ee34c31577792be0035bfa69f1f057a930715c
Zack Moscow <zmoscow@gmail.com>
2020-01-13 12:40:42 -0600

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
